dbdcc9bca95590825e259c88bc4a5254912281b9
tinta2510 <tatrungtin2004@gmail.com>
2024-11-21 20:34:31 +0700

    add vercel.json

---
+++ src/routes/index.ts
@@ -0,0 +1,10 @@
+import { Router, Request, Response } from 'express';
+
+const router = Router();
+
+router.get("/", (req: Request, res: Response) => {
+  res.json({ message: "Hello, Tin Ta!"})
+})
+
+
+export default router;
