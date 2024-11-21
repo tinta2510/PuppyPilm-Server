de849a8dc0fbae9833a6768a94a62dec41b746f6
tinta2510 <tatrungtin2004@gmail.com>
2024-11-21 22:32:58 +0700

    Test vercel prod

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get("/", (req: Request, res: Response) => {
-  res.json({ message: "Hello everyone. I am Tin."})
+  res.json({ message: "welcome to PuppyPilm server."})
 })
 
 
